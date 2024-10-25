0b99eccde01beeab3539551ca02e1d10ed04f088
BrenoSeixas <olegarioseixasb@gmail.com>
2024-10-25 17:31:32 -0300

    Aprendendo Git

---
+++ ContaBancaria/ContaBancaria/ContaBancaria.cs
@@ -0,0 +1,82 @@
+﻿namespace Teste001
+{
+    internal class ContaBancaria
+    {
+        private int _numeroDaConta { get; set; }
+        private string _nomeTitular { get; set; }
+        public char[] AbrirComSaldo { get; private set; }
+        public double Saldo { get; private set; }
+
+        public ContaBancaria()
+        {
+
+        }
+
+        public ContaBancaria(int numerodaconta, string nometitular, double saldo)
+        {
+            _numeroDaConta = numerodaconta;
+            _nomeTitular = nometitular;
+            Saldo = saldo;
+
+        }
+        public int NumeroDaconta
+        {
+            get => _numeroDaConta;
+            set
+            {
+                _numeroDaConta = value;
+            }
+        }
+
+        public string NomeTitular
+        {
+            get => _nomeTitular;
+            set
+            {
+                _nomeTitular = value;
+            }
+        }
+
+        public double AbrirSaldo(char abrir)
+        {
+
+            if (abrir == 's')
+            {
+                IniciarContaDeposito();
+            }
+            else
+            {
+                Console.WriteLine("Seu saldo é de 0,00");
+                IniciarContaSemSaldo();
+            }
+
+            return Saldo;
+        }
+
+        public double IniciarContaDeposito()
+        {
+            Console.Write("Digite quanto de saldo será adicionado: ");
+            Saldo = double.Parse(Console.ReadLine());
+            Console.WriteLine(Saldo.ToString("F2"));
+
+            return Saldo;
+        }
+
+        public double IniciarContaSemSaldo()
+        {
+            Saldo = 0;
+            return Saldo;
+        }
+
+        public void DepositarSaldo(double saldo)
+        {
+            Saldo += saldo;
+        }
+
+        public void SacarSaldo(double saldo)
+        {
+            Saldo -= saldo;
+        }
+
+    }
+}
---
+++ ContaBancaria/ContaBancaria/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net.Http.Headers;
+using System.Threading.Channels;
+using Teste001;
+
+namespace Curso
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            ContaBancaria contaBancaria = new ContaBancaria();
+
+            Console.Write("Entre com número da conta: ");
+            contaBancaria.NumeroDaconta = int.Parse(Console.ReadLine());
+            Console.WriteLine("------------------");
+
+            Console.Write("Entre com o número titular da conta: ");
+            contaBancaria.NomeTitular = Console.ReadLine();
+            Console.WriteLine("------------------");
+
+            Console.Write("Haverá deposito inicial? (s/n): ");
+            char saldoInicial = char.Parse(Console.ReadLine());
+            contaBancaria.AbrirSaldo(saldoInicial);
+
+
+            Console.WriteLine($"Dados da conta: Conta {contaBancaria.NumeroDaconta} o titular é {contaBancaria.NomeTitular} e o saldo é {contaBancaria.Saldo} ");
+            Console.WriteLine("------------------");
+
+            Console.Write("De quanto será o deposito?: ");
+            double deposito = double.Parse(Console.ReadLine());
+            contaBancaria.DepositarSaldo(deposito);
+            Console.WriteLine("------------------");
+
+            Console.WriteLine($"Dados da conta: Conta {contaBancaria.NumeroDaconta} o titular é {contaBancaria.NomeTitular} e o saldo é {contaBancaria.Saldo} ");
+
+            Console.Write("De quanto será o saque?: ");
+            double saque = double.Parse(Console.ReadLine());
+            contaBancaria.SacarSaldo(saque);
+            Console.WriteLine("------------------");
+
+            Console.WriteLine($"Dados da conta: Conta {contaBancaria.NumeroDaconta} o titular é {contaBancaria.NomeTitular} e o saldo é {contaBancaria.Saldo} ");
+        }
+    }
+}
